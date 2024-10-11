6819ce1479a750b91fc8e1a5c99045a3ff292282
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-11 11:23:02 -0700

    add routes and db connection function

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const courseRoutes = require('./routes/courseRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
+const { connectToMongoDB } = require('./models/connection');
+
 var app = express();
 
+connectToMongoDB()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/courses', courseRoutes);
+app.use('/api/categories', categoryRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
